5370dfc8f7b10019d6b7298d48ce0b0c3518a4f5
juliion <juliaionova111@gmail.com>
2022-09-06 13:40:56 +0300

    Create entities.

---
+++ LibraryDAL/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryDAL.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Cover { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Genre { get; set; } = null!;
+
+        public virtual IEnumerable<Rating> Ratings { get; set; } = null!;
+        public virtual IEnumerable<Review> Reviews { get; set; } = null!;
+    }
+}
---
+++ LibraryDAL/Entities/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryDAL.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public int BookId { get; set; }
+
+        public virtual Book Book { get; set; } = null!;
+
+    }
+}
