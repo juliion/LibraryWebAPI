87850125da5390428b037ba65e8c1725b45d79e5
juliion <juliaionova111@gmail.com>
2022-09-12 17:24:34 +0300

    Add validation for DTOs.

---
+++ LibraryBLL/Validators/RateBookDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using LibraryBLL.DTOs.Requests;
+using FluentValidation;
+
+namespace LibraryBLL.Validators
+{
+    public class RateBookDTOValidator : AbstractValidator<RateBookDTO>
+    {
+        public RateBookDTOValidator()
+        {
+            RuleFor(rb => rb.Score)
+                .NotNull()
+                .GreaterThanOrEqualTo(1)
+                .LessThanOrEqualTo(5);
+        }
+    }
+}
---
+++ LibraryBLL/Validators/SaveReviewDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using LibraryBLL.DTOs.Requests;
+using FluentValidation;
+
+namespace LibraryBLL.Validators
+{
+    public class SaveReviewDTOValidator : AbstractValidator<SaveReviewDTO>
+    {
+        public SaveReviewDTOValidator()
+        {
+            RuleFor(r => r.Message)
+                .NotNull()
+                .NotEmpty();
+            RuleFor(r => r.Reviewer)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}
