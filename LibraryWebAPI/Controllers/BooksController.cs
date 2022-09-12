8e7e6ef84a979f19c0e5302da7f3f15ef3f16188
juliion <juliaionova111@gmail.com>
2022-09-12 17:29:41 +0300

    Add BooksController.

---
+++ LibraryWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LibraryBLL.DTOs.Requests;
+using LibraryBLL.DTOs.Responses;
+using LibraryBLL.Interfaces;
+
+namespace LibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _booksService;
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+    }
+}
