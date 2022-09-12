abec7ec8b56873680e5f0ce4a302ea51efe918c1
juliion <juliaionova111@gmail.com>
2022-09-12 17:26:57 +0300

    Add interface for BooksService.
    It describes the basic methods for working with books.

---
+++ LibraryBLL/Interfaces/IBooksService.cs
@@ -0,0 +1,15 @@
+﻿using LibraryBLL.DTOs.Responses;
+using LibraryBLL.DTOs.Requests;
+
+namespace LibraryBLL.Interfaces
+{
+    public interface IBooksService
+    {
+        public IEnumerable<BookDTO>? GetAllBooksOrderBy(string? order);
+        public IEnumerable<BookDTO>? GetTop10Books(decimal minReviewsNum, string? genre);
+        public BookDetailsDTO GetBookDetails(int id);
+        public void DeleteBook(int id, string secretKey);
+        public int SaveNewBook(SaveBookDTO saveBookDTO);
+        public void RateBook(RateBookDTO rateBookDTO);
+    }
+}
